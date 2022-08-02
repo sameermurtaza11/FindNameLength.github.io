3372375362740ec92701affe64dbbc72f0df4e6f
sameermurtaza11 <sameermurtaza11@gmail.com>
2022-08-02 21:54:08 +0500

    First Task of JavaScript

---
+++ script.js
@@ -0,0 +1,4 @@
+function length(){
+    let input = prompt("Enter Your Name!!!");
+    document.getElementById("name_length").innerHTML = "Name" + " '" + input + "' " + "has " + input.length + " character" ;
+}
